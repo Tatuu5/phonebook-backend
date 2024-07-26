b866ee693edc41206d5c1af297c524cf578b6bbd
Tatuu5 <tatu_rusanen@hotmail.fi>
2024-07-26 17:39:03 +0300

    mongojs commandline ready

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://rtat:${password}@clusterforfso.i4wzbbw.mongodb.net/?retryWrites=true&w=majority&appName=ClusterForFSO`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+})
+
+if (process.argv.length > 3) {
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 3) {
+    Person.find({}).then(persons => {
+    newPersons = persons.map(person => `${person.name} ${person.number}`)
+    
+    console.log("phonebook:")
+    newPersons.forEach(person => {
+        console.log(person)
+    })
+
+    mongoose.connection.close()    
+    }
+
+    )
+}
