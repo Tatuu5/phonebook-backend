4d3dcf0a46b33a371e99281ede8de37ed48adb93
Tatuu5 <tatu_rusanen@hotmail.fi>
2024-07-29 16:08:00 +0300

    name and number validating updated

--- index.js
+++ index.js
@@ -96,14 +96,7 @@ app.post('/api/persons', (request, response, next) => {
             error: 'name or number missing'
         })
     }
-    /*
-    const nameExists = people.some(person => person.name === body.name)
-    if (nameExists) {
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-        })
-    }
-    */
+    
     const person = new Person({
     name: body.name,
     number: body.number
@@ -117,14 +110,11 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
+  
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true})
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query'})
     .then(updatedPerson => {
       response.json(updatedPerson)
       console.log(updatedPerson)
@@ -149,7 +139,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
   next(error)
 }
--- models/person.js
+++ models/person.js
@@ -14,8 +14,21 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: (v) => {
+                return /^\d{2,3}-\d{5,}/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'number is required']
+    },
 })
 
 personSchema.set('toJSON', {
